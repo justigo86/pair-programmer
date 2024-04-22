5725c83bf978f1f39bd190ab21f643103930ef5d
Otto <justigo86@gmail.com>
2024-04-22 08:04:03 -0400

    updating header - included a dropdown on user account

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,20 +3,48 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+function AccountDropdown() {
+  const session = useSession();
+  const isLoggedId = !!session.data;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger> {session.data?.user?.name}</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {session.data ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
-        )}
-        {session.data?.user?.name}
-        <ModeToggle />
+    <header className="bg-gray-50 dark:bg-gray-900 py-4 container mx-auto">
+      <div className="flex items-center justify-between">
+        <div>LOGO</div>
+        <div>
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
