bc245b02c8f557c43816e7c8676608093451f150
Otto <justigo86@gmail.com>
2024-04-23 22:22:15 -0400

    started working on main page including formatting - added toploader - adjusted index.js db info to prevent HRM error issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   //query data from db into component
   const rooms = await db.query.room.findMany();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen p-16">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl">Find Dev Rooms</h1>
+        <Button asChild>
+          <Link href="/create-room">Create Room</Link>
+        </Button>
+      </div>
       {rooms.map((room) => {
         return <div key={room.id}>{room.name}</div>;
       })}
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
 //imported to query and store data
 // import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from "postgres";
@@ -9,8 +9,25 @@ import * as schema from "./schema";
 // migrate(drizzle(migrationClient), ...)
 
 // for query purposes
-const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient, { schema });
+// const queryClient = postgres(process.env.DATABASE_URL!);
+// const db = drizzle(queryClient, { schema });
+
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+} else {
+  if (!global.db) {
+    global.db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+  }
+
+  db = global.db;
+}
+//comes from online forum - done to prevent
 
 export { db };
 // await db.select().from(...)...
